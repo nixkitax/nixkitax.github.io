9f5f3b0644445dc0395f824d9f0ab24e44c60bc8
nixkitax <nicol.eeemanuele@icloud.com>
2026-01-23 20:31:45 +0100

    Updates

--- src/data/contact.ts
+++
@@ -1,6 +0,0 @@
-export const contact = {
-  email: "nicol.eeemanuele@icloud.com",
-  github: "https://github.com/nixkitax",
-  linkedin: "https://linkedin.com/in/nicol-emanuele",
-  location: "Milan / Stockholm / Remote",
-};
